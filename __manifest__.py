c9937fbf219ea24bb81e74f67b88037e4cde486d
mikey <mikey@rig2.dom>
2019-01-02 09:03:00 +0100

    rename, refactor

--- __manifest__.py
+++ __manifest__.py
@@ -1,37 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Mrp worksheet based on sale order",
-
-    'summary': """
+        'name': "Mrp worksheet based on sale order",
+        'summary': """
         Provides raport showing all manufacturing elements related to source Sale Order
         Can be used as a working order in manufacturing plan""",
 
-    'description': """
-        Long description of module's purpose, 
-        It depends
-    """,
+        'description': """ 
+        desc:
+        Provides raport showing all manufacturing elements related to source Sale Order
+        Can be used as a working order in manufacturing plan""",
 
-    'author': "Maciej Stawicki",
-    'website': "http://3lance.pl",
+        'author': "Maciej Stawicki",
+        'website': "http://odoo.3lance.pl",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Manufacturing',
-    'version': '0.1',
+        'category': 'Manufacturing','Sale'
+        'version': '0.2',
 
-    # any module necessary for this one to work correctly
-    'depends': ['sale_mrp'],
+        # any module necessary for this one to work correctly
+        'depends': ['sale_mrp'],
 
-    'license': 'AGPL-3',
-    # always loaded
-    'data': [
-        'reports/report_so_worksheet.xml'
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-    ],
+        'license': 'AGPL-3',
+        'data': [
+            'reports/report_saleorder_worksheet.xml'
+            ],
+        'demo': [
+            ],
 
-    'installable': True,
-    'auto_install': False,
-}
+        'installable': True,
+        'auto_install': False,
+        }
